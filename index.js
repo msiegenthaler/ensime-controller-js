b7e991156801c21d34e768e762084ab0b2c5031e
Mario Siegenthaler <msiegenthaler@inventsoft.ch>
2016-01-11 22:08:53 +0100

    Calls to the launcher implemented.

--- index.js
+++ index.js
@@ -1,3 +1,66 @@
 launcher = require("ensime-launcher-js");
 
-module.exports = {};
+var initialized;
+var dotEnsime;
+
+var sbtCmd = "sbt";
+var ensimeVersion = "0.9.10-SNAPSHOT";
+
+
+function setup(dotEnsime_, ensimeInstallDir, options) {
+  dotEnsime = dotEnsime_;
+  if (options) {
+    if (options.sbt) sbtCmd = options.sbt;
+    if (options.ensimeVersion) ensimeVersion = options.ensimeVersion;
+  }
+  launcher.setup(dotEnsime_, ensimeVersion, ensimeInstallDir, sbtCmd);
+  initialized = true;
+}
+
+function connect(callback) {
+  if (!initialized) return callback("Not initialized, please call setup first.");
+
+  launcher.start(function(err, port) {
+    console.log(callback);
+    if (err) return callback(err);
+
+    //TODO
+    callback(false, "Ensime now started on port "+port);
+  });
+}
+
+function update(callback) {
+  launcher.update(callback);
+}
+
+function status(callback) {
+  if (!initialized) return callback("Not initialized, please call setup first.");
+
+  //TODO
+  callback("TODO");
+}
+
+function stop(callback) {
+  if (!initialized) return callback("Not initialized, please call setup first.");
+
+  //TODO
+  callback("TODO");
+}
+
+
+module.exports = {
+  /** Setup the library. Must be called before any other function. */
+  setup: setup,
+
+  /** Start ensime and connect to it. */
+  connect: connect,
+
+  /** Update ensime. */
+  update: update,
+
+  /** Current status of ensime. */
+  status: status,
+
+  /** Terminate ensime. */
+  stop: stop
+};
---
+++ run.sh
@@ -0,0 +1,16 @@
+// Used for testing and as an example.
+
+var ec = require("./index");
+
+if (process.argv.length < 3) {
+  return console.error("Specify the .ensime file as the first command line argument.");
+}
+var dotEnsime = process.argv[2];
+
+ec.setup(dotEnsime, "/tmp/ensime");
+
+ec.connect(function(err, res) {
+  if (err) return console.error(err);
+
+  console.info(res);
+});
